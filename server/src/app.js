89b2919f73fe0131bb89b9e7feb451255df760d6
sunG Ghostemane <songwutt64@nu.ac.th>
2023-08-17 21:16:20 +0700

    page 82

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+require('./route')(app)
+
+app.get('/status', function (req,res){
+    res.send('Hello nodejs server belong to Songwut')
+})
+
+app.get('/hello/:name', function(req,res){
+    console.log('hello - ' + req.params.name)
+    res.send('say hello with ' + req.params.name)
+})
+
+let port = 8081
+app.listen(port, function(){
+    console.log('server runing on ' + port)
+})
